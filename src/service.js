866d401598264a32e44a1c849357a80e07c9ce91
Scott González <scott.gonzalez@gmail.com>
2012-01-09 12:52:19 -0500

    Added type checks for all package.json fields.

--- src/service.js
+++ src/service.js
@@ -40,8 +40,13 @@ extend( Repo.prototype, {
 	validatePackageJson: function( package, version ) {
 		var errors = [];
 
+		/** required fields **/
+
+		// TODO: verify URL-safe characters
 		if ( !package.name ) {
 			errors.push( "Missing required field: name." );
+		} else if ( typeof package.name !== "string" ) {
+			errors.push( "Invalid data type for name; must be a string." );
 		} else if ( package.name.charAt( 0 ) === "_" || package.name.charAt( 0 ) === "." ) {
 			errors.push( "Name cannot start with an underscore or dot." );
 		// don't allow semantic version for plugin name
@@ -61,26 +66,117 @@ extend( Repo.prototype, {
 
 		if ( !package.title ) {
 			errors.push( "Missing required field: title." );
+		} else if ( typeof package.title !== "string" ) {
+			errors.push( "Invalid data type for title; must be a string." );
 		}
 
 		if ( !package.author ) {
 			errors.push( "Missing required field: author." );
 		} else if ( !package.author.name ) {
 			errors.push( "Missing required field: author.name." );
+		} else {
+			if ( typeof package.author.name !== "string" ) {
+				errors.push( "Invalid data type for author.name; must be a string." );
+			}
+			// TODO: verify email address format
+			if ( "email" in package.author && typeof package.author.email !== "string" ) {
+				errors.push( "Invalid data type for author.email; must be a string." );
+			}
+			// TODO: verify url format
+			if ( "url" in package.author && typeof package.author.url !== "string" ) {
+				errors.push( "Invalid data type for author.url; must be a string." );
+			}
 		}
 
 		if ( !package.licenses ) {
 			errors.push( "Missing required field: licenses." );
 		} else if ( !package.licenses.length ) {
 			errors.push( "There must be at least one license." );
-		} else if ( package.licenses.filter(function( license ) { return !license.url; }).length ) {
-			errors.push( "Missing required field: license.url." );
+		} else {
+			package.licenses.forEach(function( license, i ) {
+				// TDOO: verify url format
+				if ( !license.url ) {
+					errors.push( "Missing required field: licenses[" + i + "].url." );
+				}
+			});
 		}
 
 		if ( !package.dependencies ) {
 			errors.push( "Missing required field: dependencies." );
-		} else if ( !package.dependencies.jquery ) {
-			errors.push( "Missing required dependency: jquery." );
+		} else {
+			if ( !package.dependencies.jquery ) {
+				errors.push( "Missing required dependency: jquery." );
+			}
+			Object.keys( package.dependencies ).forEach(function( dependency ) {
+				// TODO: validate name
+				// TODO: validate version
+			});
+		}
+
+		/** optional fields **/
+
+		if ( "description" in package && typeof package.description !== "string" ) {
+			errors.push( "Invalid data type for description; must be a string." );
+		}
+
+		if ( "keywords" in package ) {
+			if ( !Array.isArray( package.keywords ) ) {
+				errors.push( "Invalid data type for keywords; must be an array." );
+			} else {
+				package.keywords.forEach(function( keyword, i ) {
+					// TODO: any character restrictions on keywords?
+					if ( typeof keyword !== "string" ) {
+						errors.push( "Invalid data type for keywords[" + i + "]; must be a string." );
+					}
+				});
+			}
+		}
+
+		// TODO: verify url format
+		if ( "homepage" in package && typeof package.homepage !== "string" ) {
+			errors.push( "Invalid data type for homepage; must be a string." );
+		}
+
+		// TODO: verify url format
+		if ( "docs" in package && typeof package.docs !== "string" ) {
+			errors.push( "Invalid data type for docs; must be a string." );
+		}
+
+		// TODO: verify url format
+		if ( "demo" in package && typeof package.demo !== "string" ) {
+			errors.push( "Invalid data type for demo; must be a string." );
+		}
+
+		if ( "maintainers" in package ) {
+			if ( !Array.isArray( package.maintainers ) ) {
+				errors.push( "Invalid data type for maintainers; must be an array." );
+			} else {
+				package.maintainers.forEach(function( maintainer, i ) {
+					if ( typeof maintainer.name !== "string" ) {
+						errors.push( "Invalid data type for maintainers[" + i + "].name; must be a string." );
+					}
+					// TODO: verify email address format
+					if ( "email" in maintainer && typeof maintainer.email !== "string" ) {
+						errors.push( "Invalid data type for maintainers[" + i + "].email; must be a string." );
+					}
+					// TODO: verify url format
+					if ( "url" in maintainer && typeof maintainer.url !== "string" ) {
+						errors.push( "Invalid data type for maintainers[" + i + "].url; must be a string." );
+					}
+				});
+			}
+		}
+
+		if ( "files" in package ) {
+			if ( !Array.isArray( package.files ) ) {
+				errors.push( "Invalid data type for files; must be an array." );
+			} else {
+				package.files.forEach(function( file, i ) {
+					if ( typeof file !== "string" ) {
+						errors.push( "Invalid data type for files[" + i + "]; must be a string." );
+					}
+				});
+			}
 		}
 
 		return errors;
