ddc8e64eed58dd3da9173dd5bc1317b158485b09
Scott González <scott.gonzalez@gmail.com>
2012-03-22 11:50:31 -0400

    Better support for temp directories.

--- src/config.js
+++ src/config.js
@@ -1,14 +1,15 @@
 var path = require( "path" ),
+	tmpDir = process.env.TMPDIR || process.env.TMP || process.env.TEMP ||
+		( process.platform === "win32" ? "c:/windows/temp" : "/tmp" ),
 	config = require( process.env.JQ_PATH ?
 		process.env.JQ_PATH + "/plugins.jquery.com" :
 		"../config" );
 
-console.log( config.test );
 function resolvePath( key, _default ) {
 	config[ key ] = path.resolve( __dirname, "..", config[ key ] || _default );
 }
 
-resolvePath( "repoDir", "/tmp/plugin-repos" );
+resolvePath( "repoDir", path.resolve( tmpDir, "plugin-repos" ) );
 resolvePath( "pluginsDb", "plugins.db" );
 
 module.exports = config;
