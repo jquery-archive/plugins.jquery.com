2a1adb0520040e3248e4a0d1cb20d466d3f04047
Adam Ulvi <adam@delaris.com>
2013-05-29 23:03:38 -0700

    Updated formatting per style guide.

--- scripts/github-stats.js
+++ scripts/github-stats.js
@@ -1,51 +1,39 @@
 var https = require( "https" ),
 	logger = require( "../lib/logger" );
 
-module.exports = function ( plugin, callback ) {
+module.exports = function( plugin, callback ) {
 
-	function response ( res ) {
-		var data = "",
-			error = null;
+	function response( res ) {
+		var data = "";
 
-		res.on ( "data", function(chunk){ data += chunk; } );
+		res.on( "data", function(chunk){ data += chunk; } );
 
-		res.on ( "end", function(){
-			if ( res.statusCode === 304 ) {
-				logger.log("304, no work to do");
-			} else if ( res.statusCode === 200 ) {
+		res.on( "end", function(){
+			if ( res.statusCode === 200 ) {
 				try {
-					data = JSON.parse(data);
+					data = JSON.parse( data );
 					plugin.watchers = data.watchers_count;
 					plugin.forks = data.forks_count;
-
-					logger.log("Plugin " + plugin.plugin + " updated!");
-					logger.log("Watchers: " + data.watchers_count + " Forks: " + data.forks_count + "\n");
-				} catch (err) {
-					error = err;
+					logger.log( "Updated " + plugin.plugin + ". Watchers(" + plugin.watchers + ") Forks(" + plugin.forks + ")" );
+				} catch ( e ) {
+					callback( e );
 				}
 			} else {
-				// Well that's weird, wat do?
-				logger.log("Unexpected reply, take a look:\n\n" + data);
+				logger.error( "Unexpected reply: " + data );
 			}
 
-			callback(null, plugin);
+			callback( null, plugin );
 		});
 	}
 
-	function errorHandler(error) {
-		logger.log("Error: " + error);
-	}
-
-	logger.log("Processing plugin " + plugin.plugin);
-
 	var options = {
 		host: "api.github.com",
-		path: "/repos/" + plugin.repo.split("/").slice(1).join("/"),
+		path: "/repos/" + plugin.repo.split( "/" ).slice( 1 ).join( "/" ),
 		headers: {
 			"user-agent": "stats/0.1 (+http://plugins.jquery.com)"
 		}
 	};
 
-	https.request( options, response ).on( "error", errorHandler ).end();
+	https.request( options, response ).on( "error", logger.error ).end();
 };
 
